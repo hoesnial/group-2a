5386ed286b26199491d7155d819b0af47784f04b
novaramadhannn <novaramadhan058@gmail.com>
2024-12-06 10:00:57 +0700

    Page Kegiatan

---
+++ kegiatan/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kegiatan Kolaborasi</title>
+    <link rel="stylesheet" href="kolaborasi.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link href=<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<?php include('navbar.php'); ?>
+
+    <div class="kolaborasi-container">
+        <header class="kolaborasi-header">
+            <h1>Kegiatan</h1>
+            <h2>Eksplorasi Tak Terbatas bersama Luarsekolah</h2>
+            <p>
+                Ikuti berbagai aktivitas seru yang menginspirasi dan memperkuat solidaritas. Mulai dari pelatihan, 
+                penggalangan dana, hingga kolaborasi dengan berbagai komunitas, kami berkomitmen menciptakan peluang 
+                bagi generasi muda untuk berbagi dan tumbuh bersama di setiap langkah.
+            </p>
+        </header>
+        <section class="kolaborasi-cards">
+    <div class="kolaborasi-card gambar-pertama">
+        <img src="images/Collaborative Work Session.jpeg" alt="Kegiatan Kolaborasi">
+        <h3>Kegiatan Kolaborasi</h3>
+        <p>Unity for Charity by Luarsekolah X Rumah Amal Salman</p>
+    </div>
+    <div class="kolaborasi-card">
+        <img src="images/next-academy-JyJwO0K5fWM-unsplash.jpg" alt="Magang+">
+        <h3>Magang+</h3>
+        <p>Unity for Charity by Luarsekolah X Rumah Amal Salman</p>
+    </div>
+    <div class="kolaborasi-card">
+        <img src="images/Business Meeting in Modern Conference Room.jpeg" alt="Pelatihan Prakerja">
+        <h3>Pelatihan Prakerja</h3>
+        <p>Unity for Charity by Luarsekolah X Rumah Amal Salman</p>
+    </div>
+    <div class="kolaborasi-card">
+        <img src="images/Attentive Student in Colorful Classroom.jpeg" alt="Luarsekolah Cendikia">
+        <h3>Luarsekolah Cendikia</h3>
+        <p>Unity for Charity by Luarsekolah X Rumah Amal Salman</p>
+    </div>
+</section>
+
+
+    </div>
+</body>
+</html>
