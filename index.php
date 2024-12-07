2722296ca7e83270b2322bac985d056950392c63
novaramadhannn <novaramadhan058@gmail.com>
2024-12-07 18:55:26 +0700

    Font Poppins

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <title>LuarSekolah</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+</head>
 </head>
 <body>
 
